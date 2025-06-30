347108021af28c4c09fc6a145026d93de27d13bf
miitaku <165885063+miitaku@users.noreply.github.com>
2025-06-30 22:39:24 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-  API_KEY: process.env.TWITTER_API_KEY,
-  API_SECRET: process.env.TWITTER_API_SECRET,
+  APT_KEY: process.env.TWITTER_APT_KEY,
+  APT_SECRET: process.env.TWITTER_APT_SECRET,
   ACCESS_TOKEN: process.env.TWITTER_ACCESS_TOKEN,
   ACCESS_TOKEN_SECRET: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   BEARER_TOKEN: process.env.TWITTER_BEARER_TOKEN,
